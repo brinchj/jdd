94d939d3f62029b304c0a005013e9aa486e31268
Johan Brinch <brinchj@gmail.com>
2012-07-18 14:55:21 +0200

    Derive Show

--- Jimple.hs
+++ Jimple.hs
@@ -8,12 +8,16 @@ data JimpleMethod = Method
                     [IdentStmt]
                     [(Maybe Label, Stmt)]
                     [Except]
+                  deriving Show
 
 data IdentStmt = IStmt Local Ref
+               deriving Show
 
 data LocalDecl = LocalDecl Type String
+               deriving Show
 
 data Except = Except Ref Label Label Label
+            deriving Show
 
 
 data Stmt = S_breakpoint
@@ -30,32 +34,41 @@ data Stmt = S_breakpoint
           | S_returnVoid
           | S_tableSwitch Im Label [(Int, Label)]
           | S_throw Im
+          deriving Show
 
 
 data Value = VConst Constant
            | VLocal Local
            | VExpr  Expression
+           deriving Show
 
 data Label = Label String
+           deriving Show
 
 data Im = IConst Constant
         | ILocal Local
+        deriving Show
 
 data Local = Local String
+             deriving Show
+
 data Constant = C_double Double
               | C_float  Double
               | C_int    Integer
               | C_long   Integer
               | C_string B.ByteString
               | C_null
+              deriving Show
 
 data Variable = VarRef Ref | VarLocal Local
+              deriving Show
 
 data RValue = RV_ref   Ref
             | RV_const Constant
             | RV_expr  Expression
             | RV_local Local
             | RV_nnsa  Integer -- TODO: next_next_statement_address ??
+            deriving Show
 
 
 data Ref = R_caughtException
@@ -64,6 +77,7 @@ data Ref = R_caughtException
          | R_array         Integer
          | R_instanceField Integer
          | R_staticField   Integer
+         deriving Show
 
 
 data Expression = E_eq Im Im -- Conditions
@@ -95,12 +109,16 @@ data Expression = E_eq Im Im -- Conditions
                 | E_newArray Type Im
                 | E_new Ref
                 | E_newMultiArray Type Im [Im] -- TODO: empty dims?
+                deriving Show
 
 data Invoke = I_interface Im MethodSignature [Im]
             | I_speciale  Im MethodSignature [Im]
             | I_virtual   Im MethodSignature [Im]
             | I_static MethodSignature [Im]
+            deriving Show
 
 data MethodSignature = MethodSig String [Type] Type
+                     deriving Show
 
 data Type = T_int | T_long | T_float | T_double | T_ref | T_addr | T_void
+          deriving Show
