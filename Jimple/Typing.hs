a413a9c338d9ee302253ef2c5e6cd5e86cfbbe97
Johan Brinch <brinchj@gmail.com>
2012-08-02 21:27:52 +0200

    First shot at some basic typing (and variable splitting on types)

--- Jimple/Typing.hs
+++ Jimple/Typing.hs
@@ -5,12 +5,21 @@
 
 module Jimple.Typing where
 
+import Util
 import Jimple.Types
-
+import qualified Parser as CF
 
 import Data.Either
-import qualified Data.Foldable as F
+import Data.Maybe
 
+import qualified Data.Foldable as F
+import qualified Data.Map as M
+
+import Control.Applicative
+import Control.Monad
+import qualified Control.Monad.State as ST
+
+import Debug.Trace
 
 type TypeV v = Either Type (Variable v)
 
@@ -59,8 +68,9 @@ instance (TypeableJ v v) => TypeableJ (Expression v) v where
   typeOf (E_cast t v) = Left t
   typeOf (E_instanceOf v _) = Left T_boolean
   typeOf (E_newArray t v) = Left $ T_array 1 t
-  typeOf (E_new ref) = Right $ VarRef ref
+  typeOf (E_new (R_object c)) = Left $ T_object $ CF.classPath c
   typeOf (E_newMultiArray t v _) = Left $ T_array undefined t
+  typeOf (E_invoke _ sig _) = Left $ methodResult sig
 
   -- comparisons
   typeOf e | isCmp e = Left T_boolean
@@ -73,3 +83,54 @@ instance TypeableJ Value Value where
   typeOf (VConst c) = typeOf c
   typeOf (VLocal v) = Right v
   typeOf (VExpr  e) = typeOf e
+
+
+type SimpleTyperST = (M.Map Local Type, M.Map Local Local)
+simpleTyper :: JimpleMethod Value -> JimpleMethod Value
+simpleTyper (meth@(Method ls is ms me)) =
+  meth { methodLocalDecls = ls2
+       , methodStmts = zip (map fst ms) ms2
+       }
+  where
+    ls2 = map (uncurry $ flip LocalDecl) $ M.toList types2
+
+    rMap = M.empty
+
+    (ms2, (types2, _)) = ST.runState (mapM go $ map snd ms) (types, rMap)
+    types = M.fromList $ map (\(LocalDecl t l) -> (l, t)) ls
+
+    db a = traceShow a a
+
+    go :: Stmt Value -> ST.State SimpleTyperST (Stmt Value)
+    go s = do case s of
+                S_assign (VarLocal v) e -> set v $ typeOf e
+                _ -> return ()
+              m <- ST.gets snd
+              return $ rename m `fmap` s
+
+    rename m (VLocal (VarLocal l)) = VLocal $ VarLocal $
+                                     fromMaybe l $ M.lookup l m
+    rename _ v = v
+
+    set :: Local -> TypeV Value -> ST.State SimpleTyperST ()
+    set (Local "_") _ = return ()
+    set (Local nm) (Left t) = do
+      let nms = nm:[nm ++ '_' : show i | i <- [2..]]
+      nm2 <- Local <$> findMatch t nms
+      modifySnd $ M.insert (Local nm) nm2
+    set (Local nm) (Right (VarLocal l)) = do
+      mt <- ST.gets $ M.lookup l . fst
+      case mt of
+        Nothing -> return ()
+        Just t  -> set (Local nm) $ Left t
+    set a b = error $ "not prepared for: " ++ show a ++ ", " ++ show b
+
+
+    findMatch t (nm:nms) = do
+      mt <- ST.gets $ M.lookup (Local nm) . fst
+      case mt of
+        Nothing -> do modifyFst $ M.insert (Local nm) t
+                      return nm
+        Just t2 | t == t2   -> return nm
+                | otherwise -> findMatch t nms
+
