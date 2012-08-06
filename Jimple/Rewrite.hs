eae1554a5cd7956bcaa2b55816dea3ba637fc7f6
Johan Brinch <brinchj@gmail.com>
2012-08-06 15:20:15 +0200

    Simplify

--- Jimple/Rewrite.hs
+++ Jimple/Rewrite.hs
@@ -5,6 +5,7 @@
 
 module Rewrite where
 
+import Control.Applicative hiding (many)
 import Control.Monad
 
 import Data.Maybe
@@ -31,42 +32,58 @@ goto = satisfy f
   where
     f (Nothing, S_goto _) = True
     f                   _ = False
+gotoP = snd <$> goto
+
 
 label = satisfy f
   where
     f (Just lbl, _) = True
     f             _ = False
+labelP = fst <$> label
 
 
 jumpless :: Parser Item
 jumpless = satisfy f
   where
-    f (Nothing, S_goto _) = False
-    f (Nothing, S_if _ _) = False
-    f _                   = True
+    f (_, S_goto _) = False
+    f (_, S_if _ _) = False
+    f (Nothing,  _) = True
+    f _             = False
 
 
--- Eliminate cross goto'ing (..0 -> 2..2 -> 1..1 -> 3.. => 0..1..2..3)
+-- Eliminate cross goto:
+-- goto 2
+-- 1: ...
+-- goto 3:
+-- 2: ...
+-- goto 1
+-- 3:
+-- ==>
+-- 1: ...
+-- 2: ...
+-- 3:
 elimGoto :: Parser (Int, [Item])
 elimGoto = do
-  (Nothing, S_goto lbl2)  <- goto
+  S_goto lbl2 <- gotoP
   body1Top@(Just lbl1, _) <- label
 
   body1 <- many jumpless
 
-  (Nothing, S_goto lbl3)   <- goto
+  S_goto lbl3 <- gotoP
   body2Top@(Just lbl2', _) <- label
   guard $ lbl2 == lbl2'
 
   body2 <- many jumpless
 
-  (Nothing, S_goto lbl1')  <- goto
+  S_goto lbl1' <- gotoP
   guard $ lbl1 == lbl1'
 
   body3Top@(Just lbl3', _) <- label
   guard $ lbl3 == lbl3'
 
-  let size = 6 + (length $ concat [body1, body2])
+  -- compute line size of parsed statements
+  let size = 6 + length (body1 ++ body2)
+  -- rearrange blocks without gotos
   let body = (body2Top:body2) ++ (body1Top:body1) ++ [body3Top]
 
   return (size, body)
@@ -77,6 +94,6 @@ rewrite p xs = go xs
   where
     go [] = Nothing
     go xs = case runP p () "" xs of
-      Left _            -> (head xs:) `fmap` (go $ tail xs)
+      Left _            -> (head xs:) `fmap` go (tail xs)
       Right (size, xs') -> Just $ xs' ++ drop size xs
 
