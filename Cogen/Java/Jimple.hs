2a5723bbf66c36a257155792bf0cdab390ad8d04
Johan Brinch <brinchj@gmail.com>
2012-12-27 17:06:36 +0100

    Add more types and statements to be decompiled

--- Cogen/Java/Jimple.hs
+++ Cogen/Java/Jimple.hs
@@ -41,25 +41,41 @@ str = B.unpack
 
 
 -- constant
-const (C_string s) = show s
-
+const (C_double d)  = show d
+const (C_float  f)  = show f
+const (C_int    i)  = show i
+const (C_long   l)  = show l
+const (C_string s)  = show s
+const C_null        = "NULL"
+const (C_boolean b) = show b
 
 -- types
 type_ t = case t of
   T_array n tp -> concat $ type_ tp:replicate n "[]"
   T_object cp  -> path cp
   T_void -> "void"
+  T_int  -> "int"
   foo -> show foo
 
 
 -- expression
 expr e = case e of
+  E_eq a b -> op "==" a b
+  E_ge a b -> op ">=" a b
+  E_le a b -> op "<=" a b
+  E_lt a b -> op "<"  a b
+  E_ne a b -> op "!=" a b
+  E_gt a b -> op ">"  a b
+
   E_invoke it (MethodSig _cl nm pars res) args ->
     concat [invoke it, ".", str nm, "(", intercalate "," (map value args), ")"]
 
+  where
+    op f a b = concat [value a, " ", f, " ", value b]
 
 -- invoke
 invoke (I_virtual v) = value v
+invoke (I_special v) = value v
 
 
 -- variable
@@ -77,11 +93,28 @@ value (VConst c) = const c
 value (VLocal v) = var v
 value (VExpr  e) = expr e
 
+inline s = code
+  where
+    Java code = toJava s
 
 stmtToJava (lbl, s) = case s of
+  S_nop -> Java []
+
+
   S_assign v val | var v == "_" -> line $ value val
   S_assign v val -> line $ var v ++ " = " ++ value val
+
+  S_return v   -> line $ concat ["return (", value v, ")"]
   S_returnVoid -> line "return"
+
+  S_doWhile nm body v -> Java $ [
+    JavaBlock (nm ++ ": do ") (inline body) (concat [" while (", value v, ")"])]
+
+  S_ifElse e left right -> Java $ [
+    JavaBlock (concat ["if (", expr e, ") "]) (inline left) "",
+    JavaBlock "else " (inline right) "" ]
+
+  foo -> error $ "stmtToJava: Unkown statement " ++ show foo
 
 
 instance Javable LocalDecl where
@@ -104,8 +137,8 @@ methodToJava (Method sig locals0 idents stmts excs) =
     params   = intercalate ", " [
       concat [ts, " ", nm] | (ts, nm) <- zip argTypes argNames]
 
-    header = join $ map toJava locals1
-    body   = join $ map toJava stmts
+    header = toJava locals1
+    body   = toJava stmts
 
     Java code = join [header, body]
 
