77c340f6486e83b752aa8783da4c2ff3f980dd1e
Johan Brinch <brinchj@gmail.com>
2013-01-15 17:01:16 +0100

    Update if-test to cover more cases

--- examples/TIf.java
+++ examples/TIf.java
@@ -2,22 +2,69 @@ package examples;
 
 class TIf {
 
-    public static void main(String[] args) {
-        System.out.println(TIf.check(42));
-        System.out.println(TIf.check(41));
-        System.out.println(TIf.check(43));
-        System.out.println(TIf.check(0));
-        System.out.println(TIf.check(-1));
-        System.out.println(TIf.check(100));
-        System.out.println(TIf.check(101));
-        System.out.println(TIf.check(400));
-    }
-
     public static int check(int i) {
-        if (i > 100) {
-            return 42;
+        int res = 0;
+        if (i > 10) {
+            res = 4;
         } else {
-            return 13;
+            res = 3;
+        }
+	res += i;
+	if (res % 2 == 0) {
+	    res -= 2;
+	}
+	if (res % 2 == 1) {
+	   if (res + 1 < 10) {
+             return res;
+           }
+           return res+1;
+        } else {
+           return res/2;
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println(TIf.check(-20));
+        System.out.println(TIf.check(-19));
+        System.out.println(TIf.check(-18));
+        System.out.println(TIf.check(-17));
+        System.out.println(TIf.check(-16));
+        System.out.println(TIf.check(-15));
+        System.out.println(TIf.check(-14));
+        System.out.println(TIf.check(-13));
+        System.out.println(TIf.check(-12));
+        System.out.println(TIf.check(-11));
+        System.out.println(TIf.check(-10));
+        System.out.println(TIf.check(-9));
+        System.out.println(TIf.check(-8));
+        System.out.println(TIf.check(-7));
+        System.out.println(TIf.check(-6));
+        System.out.println(TIf.check(-5));
+        System.out.println(TIf.check(-4));
+        System.out.println(TIf.check(-3));
+        System.out.println(TIf.check(-2));
+        System.out.println(TIf.check(-1));
+        System.out.println(TIf.check(0));
+        System.out.println(TIf.check(1));
+        System.out.println(TIf.check(2));
+        System.out.println(TIf.check(3));
+        System.out.println(TIf.check(4));
+        System.out.println(TIf.check(5));
+        System.out.println(TIf.check(6));
+        System.out.println(TIf.check(7));
+        System.out.println(TIf.check(8));
+        System.out.println(TIf.check(9));
+        System.out.println(TIf.check(10));
+        System.out.println(TIf.check(11));
+        System.out.println(TIf.check(12));
+        System.out.println(TIf.check(13));
+        System.out.println(TIf.check(14));
+        System.out.println(TIf.check(15));
+        System.out.println(TIf.check(16));
+        System.out.println(TIf.check(17));
+        System.out.println(TIf.check(18));
+        System.out.println(TIf.check(19));
+        System.out.println(TIf.check(20));
+    }
+
 }
